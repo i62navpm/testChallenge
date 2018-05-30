cd24311987d9ea5c1d3a81f6e937bcdf3a65dfd4
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-05-30 20:09:46 +0200

    Implementation with module pattern

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,32 @@
-function Count(input) {
-  throw new Error('Not implemented')
+function Count(input = 0) {
+  function checkErrors() {
+    if (!Number.isInteger(input)) throw new Error(`A ${typeof input} value is supplied`)
+    if (input < 0) throw new RangeError('A negative value is supplied')
+  }
+
+  function decimaltoBinary() {
+    return input.toString(2)
+  }
+
+  function countOnes(binaryNumber = '') {
+    const { length } = binaryNumber.match(/1/g) || []
+    return length
+  }
+
+  function getOnesPositions(binaryNumber = '') {
+    return binaryNumber
+      .split('')
+      .reverse()
+      .reduce((after, before, index) => (+before ? [...after, index] : after), [])
+  }
+
+  checkErrors()
+
+  const binaryNumber = decimaltoBinary()
+  const numberOnes = countOnes(binaryNumber)
+  const positionOnes = getOnesPositions(binaryNumber)
+
+  return [numberOnes, ...positionOnes]
 }
 
 module.exports = { Count }
